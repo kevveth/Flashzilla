e4d00995fbab5695ff518a626a481a4b0add8ebb
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-06-19 20:14:06 -0700

    Reset textfields after a card is added & fixed bug where card would turn red for a bit when returning to the center from the right

---
+++ DraggableModifier.swift
@@ -0,0 +1,38 @@
+//
+//  DraggableModifier.swift
+//  Flashzilla
+//
+//  Created by Kenneth Oliver Rathbun on 6/18/24.
+//
+
+import SwiftUI
+
+struct DraggableModifier: ViewModifier {
+    @Binding var offset: CGSize
+    var removal: (() -> Void)?
+
+    func body(content: Content) -> some View {
+        content
+            .offset(x: offset.width * 5)
+            .rotationEffect(.degrees(offset.width / 5))
+            .gesture(
+                DragGesture()
+                    .onChanged { gesture in
+                        offset = gesture.translation
+                    }
+                    .onEnded { _ in
+                        if abs(offset.width) > 100 {
+                            removal?()
+                        } else {
+                            offset = .zero
+                        }
+                    }
+            )
+    }
+}
+
+extension View {
+    func draggable(offset: Binding<CGSize>, removal: (() -> Void)? = nil) -> some View {
+        self.modifier(DraggableModifier(offset: offset, removal: removal))
+    }
+}
--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -16,7 +16,6 @@ struct CardView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
     @Environment(\.accessibilityVoiceOverEnabled) var accessibilityVoiceOverEnabled
 
-    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
@@ -27,12 +26,7 @@ struct CardView: View {
                             .opacity(1 - Double(abs(offset.width / 50)))
 
                 )
-                .background(
-                    accessibilityDifferentiateWithoutColor
-                        ? nil
-                        : RoundedRectangle(cornerRadius: 25)
-                            .fill(offset.width > 0 ? .green : .red)
-                )
+                .cardBackground(offset: offset, differentiateWithoutColor: accessibilityDifferentiateWithoutColor)
                 .shadow(radius: 10)
             
             VStack {
@@ -56,27 +50,44 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
-        .rotationEffect(.degrees(offset.width / 5.0))
-        .offset(x: offset.width * 5)
         .opacity(2 - Double(abs(offset.width / 50)))
         .accessibilityAddTraits(.isButton)
-        .gesture(
-            DragGesture()
-                .onChanged { gesture in
-                    offset = gesture.translation
-                }
-                .onEnded { _ in
-                    if abs(offset.width) > 100 {
-                        removal?()
-                    } else {
-                        offset = .zero
-                    }
-                }
-        )
+        .draggable(offset: $offset, removal: removal)
+
         .onTapGesture {
             isShowingAnswer.toggle()
         }
         .animation(.bouncy, value: offset)
+    }
+}
+
+struct BackgroundColorModifier: ViewModifier {
+    var offset: CGSize
+    var differentiateWithoutColor: Bool
+    var color: Color {
+        if offset.width == 0 {
+            return .clear
+        } else if offset.width > 0 {
+            return .green
+        } else {
+            return .red
+        }
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .background(
+                differentiateWithoutColor 
+                ? nil
+                : RoundedRectangle(cornerRadius: 25.0)
+                    .fill(color)
+            )
+    }
+}
+
+extension View {
+    func cardBackground(offset: CGSize, differentiateWithoutColor: Bool) -> some View {
+        self.modifier(BackgroundColorModifier(offset: offset, differentiateWithoutColor: differentiateWithoutColor))
     }
 }
 
